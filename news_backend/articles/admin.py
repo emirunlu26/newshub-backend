bfbda09b88613b124f45890b43ecb292853c1f98
emirunlu26 <emirpunlu@gmail.com>
2025-12-07 23:53:26 +0300

    Region model registered to admin site

--- news_backend/articles/admin.py
+++ news_backend/articles/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from articles.models import Category, Tag, Article, ArticleView, ArticleReaction, EditTask
+from articles.models import Category, Tag, Article, ArticleView, ArticleReaction, EditTask, Region
 
 # Register your models here.
 
 admin.site.register(Category)
 admin.site.register(Tag)
+admin.site.register(Region)
 admin.site.register(Article)
 admin.site.register(ArticleView)
 admin.site.register(ArticleReaction)
