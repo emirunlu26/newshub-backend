1dbd4aaf2accf99e582b062e32ea6672a363f94a
emirunlu26 <emirpunlu@gmail.com>
2025-12-08 15:00:00 +0300

    Insterted login_requeried decorator views in users and posts apps.

--- news_backend/posts/views.py
+++ news_backend/posts/views.py
@@ -1,24 +1,30 @@
 """View module that contains all view functions to handle requests related to posts"""
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+@login_required(login_url="users:login")
 def add_post(request):
     """View function that handles the request for an user to create a post"""
     pass
 
+@login_required(login_url="users:login")
 def view_update_delete_post(request, post_id):
     """View function that handles the request for an user to view/update/delete a specific post"""
     pass
 
+@login_required(login_url="users:login")
 def add_update_view_delete_reaction_to_post(request, post_id):
     """View function that handles the request for an user to add/view/change/delete reaction to a post"""
     pass
 
+@login_required(login_url="users:login")
 def reference_post(request, post_id):
     """View function that handles the request for an user to reference a post"""
     pass
 
+@login_required(login_url="users:login")
 def view_post_form(request):
     """View function that returns a form to the requesting user to create a post"""
     pass
--- news_backend/users/views.py
+++ news_backend/users/views.py
@@ -1,5 +1,6 @@
 """View module that contains all view functions to handle requests related to users"""
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -19,38 +20,47 @@ def view_profile_picture(request, user_id):
     """View function that returns the profile picture of a specific user"""
     pass
 
+@login_required(login_url="users:login")
 def view_following_list(request, user_id):
     """View function that returns the list of users which is followed by a specific user"""
     pass
 
+@login_required(login_url="users:login")
 def view_follower_list(request, user_id):
     """View function that returns the list of users following a specific user"""
     pass
 
+@login_required(login_url="users:login")
 def view_followed_tags(request):
     """View function that returns the list of tags which is followed by the requesting user"""
     pass
 
+@login_required(login_url="users:login")
 def view_followed_categories(request):
     """View function that returns the list of categories which is followed by the requesting user"""
     pass
 
+@login_required(login_url="users:login")
 def view_bookmarked_articles(request):
     """View function that returns the list of articles which is bookmarked by the requesting user"""
     pass
 
+@login_required(login_url="users:login")
 def view_profile_settings(request):
     """View function that returns the information about profile settings of the requesting user"""
     pass
 
+@login_required(login_url="users:login")
 def view_ui_customization_settings(request):
     """View function that returns the information about user interface customization settings of the requesting user"""
     pass
 
+@login_required(login_url="users:login")
 def subscribe_or_unsubscribe(request):
     """View function that handles the request about subscription/unsubscription of the requesting user"""
     pass
 
+@login_required(login_url="users:login")
 def follow_or_unfollow(request, user_id):
     """View the function that handles the request about requesting user following/unfollowing a specific user"""
     pass
