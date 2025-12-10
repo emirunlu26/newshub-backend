1b1a16d58e6847b2a03b675e7eb618383a8614b1
emirunlu26 <emirpunlu@gmail.com>
2025-12-10 17:35:29 +0300

    Views and urlconfgs for app articles are implemented - part 2

--- news_backend/articles/urls.py
+++ news_backend/articles/urls.py
@@ -1,16 +1,26 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 app_name = "articles"
 urlpatterns = [
+    path("create/", views.create_article, name="create-article"),
+    path("<article_id>/", views.update_or_delete_article, name="update-or-delete-article"),
     path("my-news/", views.get_my_news, name="get-my-news"),
     path("trending/", views.get_trending_articles, name="get-trending-articles"),
     path("authors/<str:slug>-<int:id>", views.get_author_by_slug_and_id, "get-author-by-slug-and-id"),
-    path("<str:type>/", views.get_article_by_type(), name="get-article-by-type"),
-    path("<str:type>/<str:slug>-<int:id>/", views.get_article_by_slug_and_id
+    path("<str:type>/", views.get_articles_by_type, name="get-article-by-type"),
+    re_path(r"^<str:type>/(?P<slug>[-a-zA-Z0-9_]+)/(?P<id>[0-9]+)$/", views.get_article_by_slug_and_id_with_type
+         , name="get-article-by-slug-and-id-with-type"),
+    re_path(r"^(?P<slug>[-a-zA-Z0-9_]+)/(?P<id>[0-9]+)$/", views.get_article_by_slug_and_id_without_type
          , name="get-article-by-slug-and-id"),
     path("region/<str:region>/", views.get_article_by_region, name="get-article-by-region"),
-    path("category/<str:category>/", views.get_article_by_parent_category, name="get-article-by-parent-category"),
-    path("category/<str:parent>/<str:sub>/", views.get_article_by_sub_category, name="get-article-by-sub-category"),
-    path("tag/<str:tag>/", views.get_article_by_tag, name="get-article-by-tag")
+    path("categories/<str:slug>/", views.get_article_by_parent_category, name="get-article-by-parent-category"),
+    path("categories/<str:parent_slug>/<str:sub_slug>/", views.get_article_by_sub_category
+         , name="get-article-by-sub-category"),
+    path("tags/<str:slug>/", views.get_article_by_tag, name="get-article-by-tag"),
+    path("tags/<str:slug>/follow/", views.follow_or_unfollow_tag, name="follow-or-unfollow-tag"),
+    path("categories/<str:slug>/follow/", views.follow_or_unfollow_category, name="follow-or-unfollow-category"),
+    path("<int:id>/bookmark/", views.bookmark_or_unbookmark_article, name="bookmark-or-unbookmark-article"),
+    path("<int:id>/react", views.view_add_update_delete_reaction_to_article
+         , name="view-add-update-delete-reaction-to-article")
 ]
--- news_backend/articles/views.py
+++ news_backend/articles/views.py
@@ -1,34 +1,64 @@
 """View file that contains all view functions to handle requests related to articles"""
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 def homepage(request):
     pass
 
-def get_article_by_type(request, type):
+@login_required(login_url="users:login")
+def create_article(request):
     pass
 
-def get_author_by_slug_and_id(request, slug, id):
+@login_required(login_url="users:login")
+def update_or_delete_article(request, id):
     pass
 
-def get_article_by_slug_and_id(request, type, slug, id):
+@login_required(login_url="users:login")
+def get_articles_by_type(request, type):
+    pass
+
+def get_article_by_slug_and_id_with_type(request, type, slug, id):
+    pass
+
+def get_article_by_slug_and_id_without_type(request, slug, id):
     pass
 
 def get_article_by_region(request, region):
     pass
 
-def get_article_by_parent_category(request, parent_category):
+def get_article_by_parent_category(request, slug):
     pass
 
-def get_article_by_sub_category(request, parent_category, sub_category):
+def get_article_by_sub_category(request, parent_slug, sub_slug):
     pass
 
-def get_article_by_tag(request, tag):
+def get_article_by_tag(request, slug):
     pass
 
+def get_author_by_slug_and_id(request, slug, id):
+    pass
+
+@login_required(login_url="users:login")
 def get_my_news(request):
     pass
 
 def get_trending_articles(request):
+    pass
+
+@login_required(login_url="users:login")
+def follow_or_unfollow_tag(request, slug):
+    pass
+
+@login_required(login_url="users:login")
+def follow_or_unfollow_category(request, slug):
+    pass
+
+@login_required(login_url="users:login")
+def bookmark_or_unbookmark_article(request, id):
+    pass
+
+@login_required(login_url="users:login")
+def view_add_update_delete_reaction_to_article(request, post_id):
     pass
