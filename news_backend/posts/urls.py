1981b7b4b4f2074d15e9e34cda8ca682c35fed73
emirunlu26 <emirpunlu@gmail.com>
2025-12-13 18:01:30 +0300

    Changed name of view functions for app posts

--- news_backend/posts/urls.py
+++ news_backend/posts/urls.py
@@ -3,13 +3,13 @@ from . import views
 
 app_name = "posts"
 urlpatterns = [
-    path("posts/create/", views.add_post, name="add-post"),
-    path("posts/<int:post_id>/comments/create/", views.add_comment, name="add-comment"),
+    path("posts/create/", views.create_post, name="create-post"),
+    path("posts/<int:post_id>/comments/create/", views.create_comment, name="create-comment"),
     path("posts/<int:post_id>/", views.view_update_delete_post, name="view-update-delete-post"),
     path("comments/<int:comment_id>", views.view_update_delete_comment, name="view-update-delete-comment"),
-    path("posts/<int:post_id>/react/", views.view_add_update_delete_reaction_to_post
-         , name="add-update-view-delete-reaction-to-post"),
-    path("comments/<int:comment_id>/react", views.view_add_update_delete_reaction_to_comment
-         , name="add-update-view-delete-reaction-to-post"),
+    path("posts/<int:post_id>/react/", views.create_view_update_delete_reaction_to_post
+         , name="create-view-update-delete-reaction-to-post"),
+    path("comments/<int:comment_id>/react", views.create_view_update_delete_reaction_to_comment
+         , name="create-view-update-delete-reaction-to-comment"),
     path("posts/<int:post_id>/reference/", views.reference_post, name="reference-post"),
 ]
--- news_backend/posts/views.py
+++ news_backend/posts/views.py
@@ -1,17 +1,18 @@
 """View module that contains all view functions to handle requests related to posts"""
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
 
 # Create your views here.
 
 @login_required(login_url="users:login")
-def add_post(request):
+def create_post(request):
     """View function that handles the request for a user to create a post"""
     pass
 
-def add_comment(request, post_id):
+@login_required(login_url="users:login")
+def create_comment(request, post_id):
     """View function that handles the request for a user to create a comment"""
-    pass
 
 @login_required(login_url="users:login")
 def view_update_delete_post(request, post_id):
@@ -24,12 +25,12 @@ def view_update_delete_comment(request, post_id):
     pass
 
 @login_required(login_url="users:login")
-def view_add_update_delete_reaction_to_post(request, post_id):
+def create_view_update_delete_reaction_to_post(request, post_id):
     """View function that handles the request for a user to add/view/change/delete reaction to a post"""
     pass
 
 @login_required(login_url="users:login")
-def view_add_update_delete_reaction_to_comment(request, post_id):
+def create_view_update_delete_reaction_to_comment(request, post_id):
     """View function that handles the request for a user to add/view/change/delete reaction to a comment"""
     pass
 
